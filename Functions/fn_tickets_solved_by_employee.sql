4d2aca3984332cedebf237e3362848899e96ffea
sanketk16 <khadke.s@northeastern.edu>
2022-04-28 20:59:04 -0400

    Function to calculate no. of tickets solved by an employee

---
+++ Functions/fn_tickets_solved_by_employee.sql
@@ -0,0 +1,14 @@
+create or replace FUNCTION FN_TICKETS_SOLVED_BY_EMP (eid NUMBER)
+RETURN NUMBER
+AS 
+cnt NUMBER;
+BEGIN
+  SELECT COALESCE(count(ticketNO), 0) INTO cnt
+  FROM employee
+  join customer_support on employee.empID = customer_support.empID
+  where customer_support.ticketStatus = 'RESOLVED'
+  HAVING employee.empID = eid
+  GROUP BY employee.empID;
+  RETURN cnt;
+  exception when no_data_found then return 0;
+END FN_TICKETS_SOLVED_BY_EMP;
